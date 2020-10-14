8d677680414ef756844eb7ae9df8a6a16046a57b
Sadekss3 <kubass337@gmail.com>
2020-10-14 20:38:14 +0200

    Create Window_Add_Client.Designer.cs

---
+++ Window_Add_Client.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace E_elektryk
+{
+    partial class Window_Add_Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Phone_Number_2 = new System.Windows.Forms.TextBox();
+            this.textBox_Phone_Number_1 = new System.Windows.Forms.TextBox();
+            this.textBox_NIP = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_Company_Name = new System.Windows.Forms.TextBox();
+            this.textBox_Email_Adress = new System.Windows.Forms.TextBox();
+            this.textBox_Adress = new System.Windows.Forms.TextBox();
+            this.textBox_PESEL = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_Lastname = new System.Windows.Forms.TextBox();
+            this.Button_Cancel_Add_Client = new System.Windows.Forms.Button();
+            this.Button_Add_New_Client = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Phone_Number_2
+            // 
+            this.textBox_Phone_Number_2.Location = new System.Drawing.Point(574, 276);
+            this.textBox_Phone_Number_2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Phone_Number_2.Name = "textBox_Phone_Number_2";
+            this.textBox_Phone_Number_2.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Phone_Number_2.TabIndex = 54;
+            // 
+            // textBox_Phone_Number_1
+            // 
+            this.textBox_Phone_Number_1.Location = new System.Drawing.Point(574, 216);
+            this.textBox_Phone_Number_1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Phone_Number_1.Name = "textBox_Phone_Number_1";
+            this.textBox_Phone_Number_1.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Phone_Number_1.TabIndex = 52;
+            // 
+            // textBox_NIP
+            // 
+            this.textBox_NIP.Location = new System.Drawing.Point(234, 216);
+            this.textBox_NIP.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_NIP.Name = "textBox_NIP";
+            this.textBox_NIP.Size = new System.Drawing.Size(148, 20);
+            this.textBox_NIP.TabIndex = 48;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(424, 276);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 16);
+            this.label9.TabIndex = 55;
+            this.label9.Text = "Telefon 2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(424, 216);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 16);
+            this.label8.TabIndex = 53;
+            this.label8.Text = "Telefon 1";
+            // 
+            // textBox_Company_Name
+            // 
+            this.textBox_Company_Name.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox_Company_Name.Location = new System.Drawing.Point(574, 36);
+            this.textBox_Company_Name.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Company_Name.Name = "textBox_Company_Name";
+            this.textBox_Company_Name.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Company_Name.TabIndex = 49;
+            // 
+            // textBox_Email_Adress
+            // 
+            this.textBox_Email_Adress.Location = new System.Drawing.Point(574, 156);
+            this.textBox_Email_Adress.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Email_Adress.Name = "textBox_Email_Adress";
+            this.textBox_Email_Adress.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Email_Adress.TabIndex = 51;
+            // 
+            // textBox_Adress
+            // 
+            this.textBox_Adress.Location = new System.Drawing.Point(574, 96);
+            this.textBox_Adress.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Adress.Name = "textBox_Adress";
+            this.textBox_Adress.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Adress.TabIndex = 50;
+            // 
+            // textBox_PESEL
+            // 
+            this.textBox_PESEL.Location = new System.Drawing.Point(234, 156);
+            this.textBox_PESEL.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_PESEL.Name = "textBox_PESEL";
+            this.textBox_PESEL.Size = new System.Drawing.Size(148, 20);
+            this.textBox_PESEL.TabIndex = 47;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(234, 36);
+            this.textBox_Name.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Name.TabIndex = 45;
+            // 
+            // textBox_Lastname
+            // 
+            this.textBox_Lastname.Location = new System.Drawing.Point(234, 96);
+            this.textBox_Lastname.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Lastname.Name = "textBox_Lastname";
+            this.textBox_Lastname.Size = new System.Drawing.Size(148, 20);
+            this.textBox_Lastname.TabIndex = 46;
+            // 
+            // Button_Cancel_Add_Client
+            // 
+            this.Button_Cancel_Add_Client.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Button_Cancel_Add_Client.Location = new System.Drawing.Point(403, 350);
+            this.Button_Cancel_Add_Client.Margin = new System.Windows.Forms.Padding(4);
+            this.Button_Cancel_Add_Client.Name = "Button_Cancel_Add_Client";
+            this.Button_Cancel_Add_Client.Size = new System.Drawing.Size(202, 64);
+            this.Button_Cancel_Add_Client.TabIndex = 44;
+            this.Button_Cancel_Add_Client.Text = "Anuluj";
+            this.Button_Cancel_Add_Client.UseVisualStyleBackColor = true;
+            // 
+            // Button_Add_New_Client
+            // 
+            this.Button_Add_New_Client.Location = new System.Drawing.Point(123, 350);
+            this.Button_Add_New_Client.Margin = new System.Windows.Forms.Padding(4);
+            this.Button_Add_New_Client.Name = "Button_Add_New_Client";
+            this.Button_Add_New_Client.Size = new System.Drawing.Size(202, 64);
+            this.Button_Add_New_Client.TabIndex = 43;
+            this.Button_Add_New_Client.Text = "Dodaj";
+            this.Button_Add_New_Client.UseVisualStyleBackColor = true;
+            this.Button_Add_New_Client.Click += new System.EventHandler(this.Button_Add_New_Client_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(424, 156);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 16);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(424, 96);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 16);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Adres";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(424, 36);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 16);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Nazwa Firmy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(79, 156);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Pesel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(79, 216);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "NIP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(79, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Nazwisko";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(84, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Imie";
+            // 
+            // Window_Add_Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_Phone_Number_2);
+            this.Controls.Add(this.textBox_Phone_Number_1);
+            this.Controls.Add(this.textBox_NIP);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_Company_Name);
+            this.Controls.Add(this.textBox_Email_Adress);
+            this.Controls.Add(this.textBox_Adress);
+            this.Controls.Add(this.textBox_PESEL);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.textBox_Lastname);
+            this.Controls.Add(this.Button_Cancel_Add_Client);
+            this.Controls.Add(this.Button_Add_New_Client);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Window_Add_Client";
+            this.Text = "Window_Add_Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Phone_Number_2;
+        private System.Windows.Forms.TextBox textBox_Phone_Number_1;
+        private System.Windows.Forms.TextBox textBox_NIP;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_Company_Name;
+        private System.Windows.Forms.TextBox textBox_Email_Adress;
+        private System.Windows.Forms.TextBox textBox_Adress;
+        private System.Windows.Forms.TextBox textBox_PESEL;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_Lastname;
+        private System.Windows.Forms.Button Button_Cancel_Add_Client;
+        private System.Windows.Forms.Button Button_Add_New_Client;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
