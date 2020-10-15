1704b7076ee4916f43c3a183159be7e8e51d0777
Sadekss3 <kubass337@gmail.com>
2020-10-15 21:38:48 +0200

    Create Window_Add_Offer.cs

---
+++ Window_Add_Offer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_elektryk
+{
+    public partial class Window_Add_Offer : Form
+    {
+        public Window_Add_Offer()
+        {
+            InitializeComponent();
+        }
+    }
+}
