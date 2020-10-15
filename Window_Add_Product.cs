4edb8174500f579588d54553677ff7d07e72efb1
Sadekss3 <kubass337@gmail.com>
2020-10-15 21:38:55 +0200

    Update Window_Add_Product.cs

--- Window_Add_Product.cs
+++ Window_Add_Product.cs
@@ -36,6 +36,7 @@ namespace E_elektryk
         {
             textBox_Product_Name.Text = _p.Nazwa.ToString();
             textBox_Manufacturer.Text = _p.Producent.ToString();
+            textBox_Catalog.Text = _p.Numer_katalogowy.ToString();
             textBox_Quantity.Text = _p.Ilość.ToString();
             textBox_Price_Net.Text = _p.Cena_netto.ToString();
             textBox_Price_Gross.Text = _p.Cena_brutto.ToString();
@@ -56,7 +57,7 @@ namespace E_elektryk
             }
         }
 
-        void adding()
+        private void adding()
         {
             produkt produkt = new produkt();
             Boolean flag = true;
@@ -64,17 +65,18 @@ namespace E_elektryk
             {
                 produkt.Nazwa = textBox_Product_Name.Text.ToString();
                 produkt.Producent = textBox_Manufacturer.Text.ToString();
+                produkt.Numer_katalogowy = textBox_Catalog.Text.ToString();
                 produkt.Ilość = System.Convert.ToDouble(textBox_Quantity.Text);
                 produkt.Jm = comboBox_Unit.Text.ToString();
                 produkt.Cena_netto = System.Convert.ToDecimal(textBox_Price_Net.Text);
                 produkt.Vat = System.Convert.ToDouble(ComboBox_Taxes.Text);
                 produkt.Cena_brutto = System.Convert.ToDecimal(textBox_Price_Gross.Text);
-                //produkt.Kategoria = comboBox_Product_Category.Text;
+                produkt.Kategoria = (int)comboBox_Product_Category.SelectedValue;
                 flag = true;
             }
             catch (Exception f)
             {
-                MessageBox.Show("Ilość musi być liczbą!");
+                MessageBox.Show("Ilość musi być liczbą","Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 flag = false;
             }
             if (flag == true)
@@ -83,7 +85,7 @@ namespace E_elektryk
                 {
                     db.produkt.Add(produkt);
                     db.SaveChanges();
-                    MessageBox.Show("Produkt dodany", "OK", MessageBoxButtons.OK);
+                    MessageBox.Show("Produkt dodany", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
@@ -97,17 +99,20 @@ namespace E_elektryk
                 produkt.ID = _p.ID;
                 produkt.Nazwa = textBox_Product_Name.Text.ToString();
                 produkt.Producent = textBox_Manufacturer.Text.ToString();
+                produkt.Numer_katalogowy = textBox_Catalog.Text.ToString();
                 produkt.Ilość = System.Convert.ToDouble(textBox_Quantity.Text);
                 produkt.Jm = comboBox_Unit.Text.ToString();
                 produkt.Cena_netto = System.Convert.ToDecimal(textBox_Price_Net.Text);
                 produkt.Vat = System.Convert.ToDouble(ComboBox_Taxes.Text);
                 produkt.Cena_brutto = System.Convert.ToDecimal(textBox_Price_Gross.Text);
-                //produkt.kategoria = comboBox_kategoria.Text.ToString();
+                produkt.Kategoria = (int)comboBox_Product_Category.SelectedValue;
+                //MessageBox.Show(kat.ToString());
+                
                 flag = true;
             }
             catch (Exception f)
             {
-                MessageBox.Show("Ilość musi być liczbą!");
+                MessageBox.Show("Ilość musi być liczbą", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 flag = false;
             }
             if (flag == true)
@@ -116,7 +121,7 @@ namespace E_elektryk
                 {
                     db.produkt.AddOrUpdate(produkt);
                     db.SaveChanges();
-                    MessageBox.Show("Produkt zmodyfikowany", "OK", MessageBoxButtons.OK);
+                    MessageBox.Show("Produkt zmodyfikowany", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
@@ -151,6 +156,12 @@ namespace E_elektryk
         private void ComboBox_Taxes_SelectedIndexChanged(object sender, EventArgs e)
         {
             add_gross_price();
+        }
+
+        private void Window_Add_Product_Load(object sender, EventArgs e)
+        {
+            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'zlecenieDataSet.kategoria_produktu' . Możesz go przenieść lub usunąć.
+            this.kategoria_produktuTableAdapter.Fill(this.zlecenieDataSet.kategoria_produktu);
         }
     }
 }
