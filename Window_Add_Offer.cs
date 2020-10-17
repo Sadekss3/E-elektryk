df8d764f3dddfb56c6e94c79223e7655d1336570
Sadekss3 <kubass337@gmail.com>
2020-10-17 19:43:38 +0200

    Update Window_Add_Offer.cs

--- Window_Add_Offer.cs
+++ Window_Add_Offer.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,37 @@ namespace E_elektryk
         public Window_Add_Offer()
         {
             InitializeComponent();
+        }
+
+        private void Window_Add_Offer_Load(object sender, EventArgs e)
+        {
+            using (zlecenieEntities db = new zlecenieEntities())
+            {
+                listView1.Items.Clear();
+                List<produkt> list = db.produkt.ToList();
+                foreach (produkt p in list)
+                {
+                    decimal value = Math.Round(p.Cena_netto * System.Convert.ToDecimal(p.Ilość), 2);
+                    ListViewItem item = new ListViewItem(p.ID.ToString());
+                    item.SubItems.Add(p.Nazwa);
+                    item.SubItems.Add(p.Producent);
+                    item.SubItems.Add(p.Numer_katalogowy);
+                    item.SubItems.Add(p.Jm);
+                    item.SubItems.Add(" ");
+                    item.SubItems.Add(p.Cena_netto.ToString() + " zł");
+                    item.SubItems.Add(p.Vat.ToString() + " %");
+                    item.SubItems.Add(p.Cena_brutto.ToString() + " zł");
+                    if (p.Kategoria != 0)
+                    {
+                        kategoria_produktu product_cat = new kategoria_produktu();
+                        product_cat = db.kategoria_produktu.Find(p.Kategoria);
+                        item.SubItems.Add(product_cat.Nazwa_kategorii.ToString());
+                    }
+                    item.SubItems.Add(value.ToString() + " zł");
+                    item.Font = new Font(item.Font, FontStyle.Regular);
+                    listView1.Items.Add(item);
+                }
+            }
         }
     }
 }
