28d4c8bbf7103c094097c67d18127cc2ce6c84c1
Sadekss3 <47303734+Sadekss3@users.noreply.github.com>
2020-10-21 21:40:14 +0200

    Update main_Window.cs

--- main_Window.cs
+++ main_Window.cs
@@ -214,5 +214,35 @@ namespace E_elektryk
         {
 
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Zlecenia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
