442a76be6eb623fa50db057c9962d2adb44ad7f6
Sadekss3 <kubass337@gmail.com>
2020-10-25 11:36:16 +0100

    Update Window_Add_Offer.cs

--- Window_Add_Offer.cs
+++ Window_Add_Offer.cs
@@ -4,19 +4,21 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace E_elektryk
 {
     
     public partial class Window_Add_Offer : Form
     {
         
-
         public Window_Add_Offer()
         {
             InitializeComponent();
@@ -47,44 +49,61 @@ namespace E_elektryk
                     listView1.Items.Add(item);
                 }
             }
-        }
+        }   // Fill listView from products DB
+
         void Add_to_offer(object sender, EventArgs e)
         {
-            ListViewItem item = (ListViewItem)listView1.SelectedItems[0].Clone();
+            ListViewItem item = (ListViewItem)listView1.SelectedItems[0].Clone();   // Clone current litView object
             DataGridViewRow item_grid = new DataGridViewRow();
             item_grid.CreateCells(dataGridView1);
-            item_grid.Cells[0].Value = item.SubItems[0].Text;
-            item_grid.Cells[1].Value = item.SubItems[1].Text;
-            item_grid.Cells[2].Value = item.SubItems[2].Text;
-            //string lot = Microsoft.VisualBasic.Interaction.InputBox("Podaj ilość: ", " ", "0");
-            decimal lot = 1;
-            item_grid.Cells[3].Value = lot.ToString();
+            item_grid.Cells[0].Value = item.SubItems[0].Text;   // Add to cells Nazwa
+            item_grid.Cells[1].Value = item.SubItems[1].Text;   // Add to cells Producent
+            item_grid.Cells[2].Value = item.SubItems[2].Text;   // Add to cells JM
+            decimal lot = 1;    
+            item_grid.Cells[3].Value = lot.ToString();  // One piece for every new Product in DataGrid
             string net_offer = TRIM_price(item.SubItems[4].Text);
-            item_grid.Cells[4].Value = item.SubItems[4].Text;
-            item_grid.Cells[5].Value = System.Convert.ToDecimal(net_offer) * System.Convert.ToDecimal(lot);
-            item_grid.Cells[6].Value = item.SubItems[5].Text;
-            string gross_offer = TRIM_price(item.SubItems[6].Text);
-            item_grid.Cells[7].Value = System.Convert.ToDecimal(gross_offer) * System.Convert.ToDecimal(lot);
-            item_grid.Cells[8].Value = item.SubItems[7].Text;
+            item_grid.Cells[4].Value = System.Convert.ToDecimal(net_offer); // Add decimal one piece net price to cells C.J
+            item_grid.Cells[5].Value = System.Convert.ToDecimal(net_offer) * System.Convert.ToDecimal(lot); // Add calculated net price for all pieces
+            item_grid.Cells[6].Value = item.SubItems[5].Text;   // Add taxe to cells VAT
+            string gross_offer = TRIM_price(item.SubItems[6].Text); 
+            item_grid.Cells[7].Value = System.Convert.ToDecimal(gross_offer) * System.Convert.ToDecimal(lot);   // Add calculated gross price for all pieces
+            item_grid.Cells[8].Value = item.SubItems[7].Text;   // Add category name to cells Kategoria
             dataGridView1.Rows.Add(item_grid);
-        }
+        }   // Clone listView object and add it to dataGrid row
+
         string TRIM_price(string net)
         {
             net = net.Trim(' ', 'z', 'ł');
             return net;
-        }
+        }   // Trim char from price for decimal calculation
 
         private void dataGridView1_CellValidated(object sender, DataGridViewCellEventArgs e)
         {
             DataGridViewRow row = dataGridView1.CurrentRow;
-            decimal lot = System.Convert.ToDecimal(row.Cells[3].Value);
-            string piece_price = TRIM_price(row.Cells[4].Value.ToString());
-            row.Cells[5].Value = System.Convert.ToDecimal(piece_price) * System.Convert.ToDecimal(lot);
-            string gross_offer = TRIM_price(row.Cells[7].Value.ToString());
-            string vat = (row.Cells[6].Value.ToString()).Trim(' ', '%');
-            decimal Gross = System.Convert.ToDecimal(piece_price) + (System.Convert.ToDecimal(piece_price) * (System.Convert.ToDecimal(vat) / 100));
-            row.Cells[7].Value = Gross * lot;
-        }
+            if (row.Cells[3].Value.ToString().Contains('.'))
+            {
+                MessageBox.Show("W wartościach numerycznych użyj znaku ',' zamiast '.'", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                decimal lot = System.Convert.ToDecimal(row.Cells[3].Value);
+                string piece_price = TRIM_price(row.Cells[4].Value.ToString());
+                row.Cells[5].Value = System.Convert.ToDecimal(piece_price) * System.Convert.ToDecimal(lot);
+                string gross_offer = TRIM_price(row.Cells[7].Value.ToString());
+                string vat = (row.Cells[6].Value.ToString()).Trim(' ', '%');
+                decimal Gross = System.Convert.ToDecimal(piece_price) + (System.Convert.ToDecimal(piece_price) * (System.Convert.ToDecimal(vat) / 100));
+                row.Cells[7].Value = Gross * lot;
+                decimal sum_e_taxes = 0;
+                decimal sum_w_taxes = 0;
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    sum_e_taxes += System.Convert.ToDecimal(dataGridView1.Rows[i].Cells[5].Value);
+                    sum_w_taxes += System.Convert.ToDecimal(dataGridView1.Rows[i].Cells[7].Value);
+                    sum_e_taxes_label.Text = "Suma Netto: " + sum_e_taxes.ToString();
+                    sum_w_taxes_label.Text = "Suma Brutto: " + sum_w_taxes.ToString();
+                }
+            }
+        }   // Update dataGrid net and gross price value after enter new quantity value
     }
 }
 
