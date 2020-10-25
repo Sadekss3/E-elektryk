529a94ae4372c89162fa758e218fb3b0fad99cd3
Sadekss3 <kubass337@gmail.com>
2020-10-25 19:55:36 +0100

    Update Window_Add_Client.cs

--- Window_Add_Client.cs
+++ Window_Add_Client.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Migrations;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,16 +18,16 @@ namespace E_elektryk
         string _type;
         kontrahent _k;
 
-        public Window_Add_Client()
+        public Window_Add_Client()  
         {
             InitializeComponent();
-            _type = "Dodawanie";
+            _type = "Add";
         }
 
         public Window_Add_Client(kontrahent k)
         {
             InitializeComponent();
-            _type = "Modyfikacja";
+            _type = "Modyfication";
             _k = k;
             Button_Add_New_Client.Text = "Zmień";
             Add_Client_Information();
@@ -39,7 +40,7 @@ namespace E_elektryk
             textBox_PESEL.Text = _k.Pesel.ToString();
             textBox_NIP.Text = _k.NIP.ToString();
             textBox_Adress.Text = _k.Adres.ToString();
-            textBox_Company_Name.Text = _k.ToString();
+            textBox_Company_Name.Text = _k.Nazwa_Firmy.ToString();
             textBox_Email_Adress.Text = _k.E_mail.ToString();
             textBox_Phone_Number_1.Text = _k.Telefon_1.ToString();
             textBox_Phone_Number_2.Text = _k.Telefon_2.ToString();
@@ -47,7 +48,7 @@ namespace E_elektryk
 
         private void Button_Add_New_Client_Click(object sender, EventArgs e)
         {
-            if (_type == "Dodawanie")
+            if (_type == "Add")
             {
                 Add_New_Client();
             }
@@ -76,7 +77,7 @@ namespace E_elektryk
             }
             catch (Exception f)
             {
-                MessageBox.Show("Podano nieprawidłowy numer NIP lub PESEL");
+                MessageBox.Show("Podano nieprawidłowy numer NIP lub PESEL", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 flag = false;
             }
             if (flag == true)
@@ -85,14 +86,43 @@ namespace E_elektryk
                 {
                     db.kontrahent.Add(kontrahent);
                     db.SaveChanges();
-                    MessageBox.Show("Kontrahent dodany", "OK", MessageBoxButtons.OK);
+                    MessageBox.Show("Kontrahent dodany", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
 
         private void Modyficate_Client_Info()
         {
-
+            kontrahent kontrahent = new kontrahent();
+            Boolean flag = true;
+            try
+            {
+                kontrahent.ID = _k.ID;
+                kontrahent.Imie = textBox_Name.Text;
+                kontrahent.Nazwisko = textBox_Lastname.Text;
+                kontrahent.NIP = System.Convert.ToInt64(textBox_NIP.Text);
+                kontrahent.Pesel = System.Convert.ToInt64(textBox_PESEL.Text);
+                kontrahent.Nazwa_Firmy = textBox_Company_Name.Text;
+                kontrahent.Adres = textBox_Adress.Text;
+                kontrahent.E_mail = textBox_Email_Adress.Text;
+                kontrahent.Telefon_1 = textBox_Phone_Number_1.Text;
+                kontrahent.Telefon_2 = textBox_Phone_Number_2.Text;
+                flag = true;
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("Podano nieprawidłowy numer NIP lub PESEL", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                flag = false;
+            }
+            if (flag == true)
+            {
+                using (zlecenieEntities db = new zlecenieEntities())
+                {
+                    db.kontrahent.AddOrUpdate(kontrahent);
+                    db.SaveChanges();
+                    MessageBox.Show("Dane kontrahenta zmienione", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
         }
     }
 }
