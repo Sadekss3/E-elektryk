90283eb4ddddfed6fa280bc2c6b758bfe51fcea1
Sadekss3 <kubass337@gmail.com>
2020-10-25 19:56:01 +0100

    Create Window_Choice_Client_For_Offer.Designer.cs

---
+++ Window_Choice_Client_For_Offer.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace E_elektryk
+{
+    partial class Window_Choice_Client_For_Offer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Client_list = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Imie = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nazwisko = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Pesel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NIP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nazwa_Firmy = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Adres = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Email = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Telefon_1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Telefon_2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Client_list
+            // 
+            this.Client_list.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.Client_list.AllowColumnReorder = true;
+            this.Client_list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Client_list.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Imie,
+            this.Nazwisko,
+            this.Pesel,
+            this.NIP,
+            this.nazwa_Firmy,
+            this.Adres,
+            this.Email,
+            this.Telefon_1,
+            this.Telefon_2});
+            this.Client_list.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Client_list.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Client_list.FullRowSelect = true;
+            this.Client_list.GridLines = true;
+            this.Client_list.HideSelection = false;
+            this.Client_list.Location = new System.Drawing.Point(-1, 37);
+            this.Client_list.MultiSelect = false;
+            this.Client_list.Name = "Client_list";
+            this.Client_list.Size = new System.Drawing.Size(1666, 713);
+            this.Client_list.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.Client_list.TabIndex = 22;
+            this.Client_list.UseCompatibleStateImageBehavior = false;
+            this.Client_list.View = System.Windows.Forms.View.Details;
+            this.Client_list.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Client_list_MouseDoubleClick);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // Imie
+            // 
+            this.Imie.Text = "Imie";
+            this.Imie.Width = 120;
+            // 
+            // Nazwisko
+            // 
+            this.Nazwisko.Text = "Nazwisko";
+            this.Nazwisko.Width = 120;
+            // 
+            // Pesel
+            // 
+            this.Pesel.Text = "Pesel";
+            this.Pesel.Width = 130;
+            // 
+            // NIP
+            // 
+            this.NIP.Text = "NIP";
+            this.NIP.Width = 130;
+            // 
+            // nazwa_Firmy
+            // 
+            this.nazwa_Firmy.Text = "Nazwa Firmy";
+            this.nazwa_Firmy.Width = 250;
+            // 
+            // Adres
+            // 
+            this.Adres.Text = "Adres";
+            this.Adres.Width = 400;
+            // 
+            // Email
+            // 
+            this.Email.Text = "Adres E-mail";
+            this.Email.Width = 160;
+            // 
+            // Telefon_1
+            // 
+            this.Telefon_1.Text = "Telefon 1";
+            this.Telefon_1.Width = 120;
+            // 
+            // Telefon_2
+            // 
+            this.Telefon_2.Text = "Telefon 2";
+            this.Telefon_2.Width = 120;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.Color.LightGray;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.textBox6);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.textBox5);
+            this.panel3.Controls.Add(this.textBox4);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(-1, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1680, 46);
+            this.panel3.TabIndex = 26;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(731, 10);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(133, 20);
+            this.textBox6.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.LightGray;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(630, 10);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "ID Kontrahenta:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.LightGray;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(360, 10);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Nazwa Konbtrahenta:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(493, 10);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(133, 20);
+            this.textBox5.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(223, 10);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(133, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LightGray;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(140, 10);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Nazwa firmy:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.LightGray;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(5, 11);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Szukaj po:";
+            // 
+            // Window_Choice_Client_For_Offer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1534, 750);
+            this.Controls.Add(this.Client_list);
+            this.Controls.Add(this.panel3);
+            this.Name = "Window_Choice_Client_For_Offer";
+            this.Text = "Window_Choice_Client_For_Offer";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView Client_list;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Imie;
+        private System.Windows.Forms.ColumnHeader Nazwisko;
+        private System.Windows.Forms.ColumnHeader Pesel;
+        private System.Windows.Forms.ColumnHeader NIP;
+        private System.Windows.Forms.ColumnHeader nazwa_Firmy;
+        private System.Windows.Forms.ColumnHeader Adres;
+        private System.Windows.Forms.ColumnHeader Email;
+        private System.Windows.Forms.ColumnHeader Telefon_1;
+        private System.Windows.Forms.ColumnHeader Telefon_2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
