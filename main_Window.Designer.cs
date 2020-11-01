a300089c0f3594dc3290a8db92a869959d25f551
Sadekss3 <47303734+Sadekss3@users.noreply.github.com>
2020-11-01 22:13:09 +0100

    Update main_Window.Designer.cs

--- main_Window.Designer.cs
+++ main_Window.Designer.cs
@@ -79,11 +79,11 @@
             this.Button_DataMod_Client = new System.Windows.Forms.Button();
             this.Button_Add_Client = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox_client_id_search = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox_client_name_search = new System.Windows.Forms.TextBox();
+            this.textBox_client_company_search = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.Magazyn = new System.Windows.Forms.TabPage();
@@ -104,13 +104,11 @@
             this.Button_Add = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
-            this.label13 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox_product_symbol_search = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox_product_producent_search = new System.Windows.Forms.TextBox();
+            this.textBox_product_name_search = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.Faktury = new System.Windows.Forms.TabPage();
@@ -646,11 +644,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel3.BackColor = System.Drawing.Color.LightGray;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Controls.Add(this.textBox6);
+            this.panel3.Controls.Add(this.textBox_client_id_search);
             this.panel3.Controls.Add(this.label8);
             this.panel3.Controls.Add(this.label7);
-            this.panel3.Controls.Add(this.textBox5);
-            this.panel3.Controls.Add(this.textBox4);
+            this.panel3.Controls.Add(this.textBox_client_name_search);
+            this.panel3.Controls.Add(this.textBox_client_company_search);
             this.panel3.Controls.Add(this.label6);
             this.panel3.Controls.Add(this.label5);
             this.panel3.Location = new System.Drawing.Point(0, 0);
@@ -659,20 +657,21 @@
             this.panel3.Size = new System.Drawing.Size(1680, 46);
             this.panel3.TabIndex = 21;
             // 
-            // textBox6
+            // textBox_client_id_search
             // 
-            this.textBox6.Location = new System.Drawing.Point(731, 10);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(133, 20);
-            this.textBox6.TabIndex = 23;
+            this.textBox_client_id_search.Location = new System.Drawing.Point(747, 10);
+            this.textBox_client_id_search.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_client_id_search.Name = "textBox_client_id_search";
+            this.textBox_client_id_search.Size = new System.Drawing.Size(133, 20);
+            this.textBox_client_id_search.TabIndex = 23;
+            this.textBox_client_id_search.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.LightGray;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.Location = new System.Drawing.Point(630, 10);
+            this.label8.Location = new System.Drawing.Point(646, 10);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(97, 13);
@@ -687,25 +686,28 @@
             this.label7.Location = new System.Drawing.Point(360, 10);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(129, 13);
+            this.label7.Size = new System.Drawing.Size(145, 13);
             this.label7.TabIndex = 21;
-            this.label7.Text = "Nazwa Konbtrahenta:";
+            this.label7.Text = "Nazwisko Konbtrahenta:";
+            this.label7.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
-            // textBox5
+            // textBox_client_name_search
             // 
-            this.textBox5.Location = new System.Drawing.Point(493, 10);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(133, 20);
-            this.textBox5.TabIndex = 20;
+            this.textBox_client_name_search.Location = new System.Drawing.Point(509, 10);
+            this.textBox_client_name_search.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_client_name_search.Name = "textBox_client_name_search";
+            this.textBox_client_name_search.Size = new System.Drawing.Size(133, 20);
+            this.textBox_client_name_search.TabIndex = 20;
+            this.textBox_client_name_search.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
-            // textBox4
+            // textBox_client_company_search
             // 
-            this.textBox4.Location = new System.Drawing.Point(223, 10);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(133, 20);
-            this.textBox4.TabIndex = 19;
+            this.textBox_client_company_search.Location = new System.Drawing.Point(223, 10);
+            this.textBox_client_company_search.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_client_company_search.Name = "textBox_client_company_search";
+            this.textBox_client_company_search.Size = new System.Drawing.Size(133, 20);
+            this.textBox_client_company_search.TabIndex = 19;
+            this.textBox_client_company_search.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
             // label6
             // 
@@ -888,13 +890,11 @@
             this.panel2.BackColor = System.Drawing.Color.LightGray;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel2.Controls.Add(this.checkBox1);
-            this.panel2.Controls.Add(this.textBox10);
-            this.panel2.Controls.Add(this.label13);
-            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.textBox_product_symbol_search);
             this.panel2.Controls.Add(this.label9);
             this.panel2.Controls.Add(this.label10);
-            this.panel2.Controls.Add(this.textBox8);
-            this.panel2.Controls.Add(this.textBox9);
+            this.panel2.Controls.Add(this.textBox_product_producent_search);
+            this.panel2.Controls.Add(this.textBox_product_name_search);
             this.panel2.Controls.Add(this.label11);
             this.panel2.Controls.Add(this.label12);
             this.panel2.Location = new System.Drawing.Point(0, 0);
@@ -906,40 +906,21 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(1004, 11);
+            this.checkBox1.Location = new System.Drawing.Point(749, 11);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(129, 17);
             this.checkBox1.TabIndex = 33;
             this.checkBox1.Text = "Pokaż nieaktywne";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // textBox10
+            // textBox_product_symbol_search
             // 
-            this.textBox10.Location = new System.Drawing.Point(802, 8);
-            this.textBox10.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(133, 20);
-            this.textBox10.TabIndex = 32;
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.BackColor = System.Drawing.Color.LightGray;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label13.Location = new System.Drawing.Point(733, 10);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(65, 13);
-            this.label13.TabIndex = 31;
-            this.label13.Text = "Kategoria:";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(595, 8);
-            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(133, 20);
-            this.textBox7.TabIndex = 30;
+            this.textBox_product_symbol_search.Location = new System.Drawing.Point(595, 8);
+            this.textBox_product_symbol_search.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_product_symbol_search.Name = "textBox_product_symbol_search";
+            this.textBox_product_symbol_search.Size = new System.Drawing.Size(133, 20);
+            this.textBox_product_symbol_search.TabIndex = 30;
+            this.textBox_product_symbol_search.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
             // label9
             // 
@@ -965,21 +946,23 @@
             this.label10.TabIndex = 28;
             this.label10.Text = "Producent:";
             // 
-            // textBox8
+            // textBox_product_producent_search
             // 
-            this.textBox8.Location = new System.Drawing.Point(403, 8);
-            this.textBox8.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(133, 20);
-            this.textBox8.TabIndex = 27;
+            this.textBox_product_producent_search.Location = new System.Drawing.Point(403, 8);
+            this.textBox_product_producent_search.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_product_producent_search.Name = "textBox_product_producent_search";
+            this.textBox_product_producent_search.Size = new System.Drawing.Size(133, 20);
+            this.textBox_product_producent_search.TabIndex = 27;
+            this.textBox_product_producent_search.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
-            // textBox9
+            // textBox_product_name_search
             // 
-            this.textBox9.Location = new System.Drawing.Point(193, 7);
-            this.textBox9.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(133, 20);
-            this.textBox9.TabIndex = 26;
+            this.textBox_product_name_search.Location = new System.Drawing.Point(193, 7);
+            this.textBox_product_name_search.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_product_name_search.Name = "textBox_product_name_search";
+            this.textBox_product_name_search.Size = new System.Drawing.Size(133, 20);
+            this.textBox_product_name_search.TabIndex = 26;
+            this.textBox_product_name_search.TextChanged += new System.EventHandler(this.Form1_Load);
             // 
             // label11
             // 
@@ -1220,23 +1203,21 @@
         private System.Windows.Forms.ColumnHeader columnHeader18;
         private System.Windows.Forms.ColumnHeader columnHeader19;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox_client_id_search;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox_client_name_search;
+        private System.Windows.Forms.TextBox textBox_client_company_search;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox_product_symbol_search;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox_product_producent_search;
+        private System.Windows.Forms.TextBox textBox_product_name_search;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TextBox textBox10;
-        private System.Windows.Forms.Label label13;
     }
 }
 
