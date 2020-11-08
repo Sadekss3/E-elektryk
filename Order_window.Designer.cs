e641c09f67d48b7ccfda9ca14b827cf89ff325e9
Sadekss3 <47303734+Sadekss3@users.noreply.github.com>
2020-11-08 20:43:40 +0100

    Create Order_window.Designer.cs

---
+++ Order_window.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace E_elektryk
+{
+    partial class Order_window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Offer_Search_Offer_Name = new System.Windows.Forms.TextBox();
+            this.Offer_Search_Client_ID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Offer_Search_Client_Name = new System.Windows.Forms.TextBox();
+            this.panel_Offer = new System.Windows.Forms.Panel();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.panel_Offer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(41)))), ((int)(((byte)(166)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Offer_Search_Offer_Name);
+            this.panel1.Controls.Add(this.Offer_Search_Client_ID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Offer_Search_Client_Name);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1763, 52);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(41)))), ((int)(((byte)(166)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(72, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Nazwa zlecenia:";
+            // 
+            // Offer_Search_Offer_Name
+            // 
+            this.Offer_Search_Offer_Name.Location = new System.Drawing.Point(176, 17);
+            this.Offer_Search_Offer_Name.Margin = new System.Windows.Forms.Padding(2);
+            this.Offer_Search_Offer_Name.Name = "Offer_Search_Offer_Name";
+            this.Offer_Search_Offer_Name.Size = new System.Drawing.Size(133, 20);
+            this.Offer_Search_Offer_Name.TabIndex = 20;
+            // 
+            // Offer_Search_Client_ID
+            // 
+            this.Offer_Search_Client_ID.Location = new System.Drawing.Point(685, 17);
+            this.Offer_Search_Client_ID.Margin = new System.Windows.Forms.Padding(2);
+            this.Offer_Search_Client_ID.Name = "Offer_Search_Client_ID";
+            this.Offer_Search_Client_ID.Size = new System.Drawing.Size(133, 20);
+            this.Offer_Search_Client_ID.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(-153, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Szukaj po:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(41)))), ((int)(((byte)(166)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(584, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "ID Kontrahenta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(41)))), ((int)(((byte)(166)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(314, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Nazwa Konbtrahenta:";
+            // 
+            // Offer_Search_Client_Name
+            // 
+            this.Offer_Search_Client_Name.Location = new System.Drawing.Point(447, 17);
+            this.Offer_Search_Client_Name.Margin = new System.Windows.Forms.Padding(2);
+            this.Offer_Search_Client_Name.Name = "Offer_Search_Client_Name";
+            this.Offer_Search_Client_Name.Size = new System.Drawing.Size(133, 20);
+            this.Offer_Search_Client_Name.TabIndex = 21;
+            // 
+            // panel_Offer
+            // 
+            this.panel_Offer.Controls.Add(this.listView3);
+            this.panel_Offer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Offer.Location = new System.Drawing.Point(0, 52);
+            this.panel_Offer.Name = "panel_Offer";
+            this.panel_Offer.Size = new System.Drawing.Size(1763, 937);
+            this.panel_Offer.TabIndex = 2;
+            // 
+            // listView3
+            // 
+            this.listView3.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView3.AllowColumnReorder = true;
+            this.listView3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.listView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView3.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listView3.FullRowSelect = true;
+            this.listView3.GridLines = true;
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(0, 0);
+            this.listView3.MultiSelect = false;
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(1763, 937);
+            this.listView3.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView3.TabIndex = 22;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // Order_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1763, 989);
+            this.Controls.Add(this.panel_Offer);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Order_window";
+            this.Text = "Order_window";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel_Offer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Offer_Search_Offer_Name;
+        private System.Windows.Forms.TextBox Offer_Search_Client_ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Offer_Search_Client_Name;
+        private System.Windows.Forms.Panel panel_Offer;
+        private System.Windows.Forms.ListView listView3;
+    }
+}
