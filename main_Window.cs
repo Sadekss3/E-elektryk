3af00050f2f4c15a3db1e24304fb9b3eb75e0b23
Sadekss3 <47303734+Sadekss3@users.noreply.github.com>
2020-11-08 20:43:29 +0100

    Update main_Window.cs

--- main_Window.cs
+++ main_Window.cs
@@ -13,6 +13,8 @@ namespace E_elektryk
 {
     public partial class main_Window : Form
     {
+        int Selected_Product_ID;
+
         public main_Window()
         {
             login();
@@ -27,56 +29,50 @@ namespace E_elektryk
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Product_List_Update();
-            Client_List_Update();
-            Offer_List_Update();
+            Menu_Design();
+            //Product_List_Update();
+            //Client_List_Update();
+            //Offer_List_Update();
         }
 
-        private void Product_List_Update()
+        /*private void Product_List_Update()
         {
             Cursor.Current = Cursors.WaitCursor;
             using (zlecenieEntities db = new zlecenieEntities())
             {
-                try
+                Products_list.Items.Clear();
+                List<produkt> list = db.produkt.ToList();
+                foreach (produkt p in list.Where(lvi => lvi.Nazwa.ToLower().Contains(textBox_product_name_search.Text.ToLower()) && lvi.Producent.ToLower().Contains(textBox_product_producent_search.Text.ToLower()) && lvi.Numer_katalogowy.ToLower().Contains(textBox_product_symbol_search.Text.ToLower())))
                 {
-                    Products_list.Items.Clear();
-                    List<produkt> list = db.produkt.ToList();
-                    foreach (produkt p in list.Where(lvi => lvi.Nazwa.ToLower().Contains(textBox_product_name_search.Text.ToLower()) && lvi.Producent.ToLower().Contains(textBox_product_producent_search.Text.ToLower()) && lvi.Numer_katalogowy.ToLower().Contains(textBox_product_symbol_search.Text.ToLower())))
+                    decimal value = Math.Round(p.Cena_netto * System.Convert.ToDecimal(p.Ilość), 2);
+                    ListViewItem item = new ListViewItem(p.ID.ToString());
+                    item.SubItems.Add(p.Nazwa);
+                    item.SubItems.Add(p.Producent);
+                    item.SubItems.Add(p.Numer_katalogowy);
+                    item.SubItems.Add(p.Jm);
+                    item.SubItems.Add(p.Ilość.ToString());
+                    item.SubItems.Add(p.Cena_netto.ToString() + " zł");
+                    item.SubItems.Add(p.Vat.ToString() + " %");
+                    item.SubItems.Add(p.Cena_brutto.ToString() + " zł");
+                    if (p.Kategoria != 0)
                     {
-                        decimal value = Math.Round(p.Cena_netto * System.Convert.ToDecimal(p.Ilość), 2);
-                        ListViewItem item = new ListViewItem(p.ID.ToString());
-                        item.SubItems.Add(p.Nazwa);
-                        item.SubItems.Add(p.Producent);
-                        item.SubItems.Add(p.Numer_katalogowy);
-                        item.SubItems.Add(p.Jm);
-                        item.SubItems.Add(p.Ilość.ToString());
-                        item.SubItems.Add(p.Cena_netto.ToString() + " zł");
-                        item.SubItems.Add(p.Vat.ToString() + " %");
-                        item.SubItems.Add(p.Cena_brutto.ToString() + " zł");
-                        if (p.Kategoria != 0)
-                        {
-                            kategoria_produktu product_cat = new kategoria_produktu();
-                            product_cat = db.kategoria_produktu.Find(p.Kategoria);
-                            item.SubItems.Add(product_cat.Nazwa_kategorii.ToString());
-                        }
-                        item.SubItems.Add(value.ToString() + " zł");
-                        item.Font = new Font(item.Font, FontStyle.Regular);
-                        Products_list.Items.Add(item);
+                        kategoria_produktu product_cat = new kategoria_produktu();
+                        product_cat = db.kategoria_produktu.Find(p.Kategoria);
+                        item.SubItems.Add(product_cat.Nazwa_kategorii.ToString());
                     }
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Brak połączenia do bazy danych", "Błąd Bazy Danych", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    item.SubItems.Add(value.ToString() + " zł");
+                    item.Font = new Font(item.Font, FontStyle.Regular);
+                    Products_list.Items.Add(item);
                 }
             }
             Cursor.Current = Cursors.Default;
-        } // Add Products information from DB to listView
+        } // Add Products information from DB to listView*/
 
-        private void Button_Add_Click(object sender, EventArgs e)
+        /*private void Button_Add_Click(object sender, EventArgs e)
         {
             Window_Add_Product window_add = new Window_Add_Product();
             window_add.ShowDialog();
-        } // Add new Product
+        } // Add new Product*/
 
         private void Button_Modify_Click(object sender, EventArgs e)
         {
@@ -116,7 +112,7 @@ namespace E_elektryk
             }
         } // Delete product 
 
-        private void Client_List_Update()
+        /*private void Client_List_Update()
         {
             Cursor.Current = Cursors.WaitCursor;
             using (zlecenieEntities db = new zlecenieEntities())
@@ -139,13 +135,13 @@ namespace E_elektryk
                             Client_Adress = db.adres.Find(C.Adres);
                             if (Client_Adress.Numer_mieszkania == null)
                             {
-                                item.SubItems.Add(Client_Adress.Miasto.ToString() + " " + Client_Adress.Kod_pocztowy.ToString().Remove(2, 3)+ "-" + Client_Adress.Kod_pocztowy.ToString().Remove(0, 2) + ", " 
+                                item.SubItems.Add(Client_Adress.Miasto.ToString() + " " + Client_Adress.Kod_pocztowy.ToString().Remove(2, 3) + "-" + Client_Adress.Kod_pocztowy.ToString().Remove(0, 2) + ", "
                                     + Client_Adress.Nazwa_ulicy.ToString() + " " + Client_Adress.Numer_budynku.ToString() + " " + Client_Adress.Państwo.ToString());
                             }
                             else
                             {
-                                item.SubItems.Add(Client_Adress.Miasto.ToString() + " " + Client_Adress.Kod_pocztowy.ToString().Remove(2, 3) + "-" + Client_Adress.Kod_pocztowy.ToString().Remove(0, 2) + ", " 
-                                    + Client_Adress.Nazwa_ulicy.ToString() + " " + Client_Adress.Numer_budynku.ToString() +"/"+ Client_Adress.Numer_mieszkania.ToString() + " " + Client_Adress.Państwo.ToString());
+                                item.SubItems.Add(Client_Adress.Miasto.ToString() + " " + Client_Adress.Kod_pocztowy.ToString().Remove(2, 3) + "-" + Client_Adress.Kod_pocztowy.ToString().Remove(0, 2) + ", "
+                                    + Client_Adress.Nazwa_ulicy.ToString() + " " + Client_Adress.Numer_budynku.ToString() + "/" + Client_Adress.Numer_mieszkania.ToString() + " " + Client_Adress.Państwo.ToString());
                             }
                         }
                         item.SubItems.Add(C.E_mail);
@@ -160,13 +156,13 @@ namespace E_elektryk
                 }
             }
             Cursor.Current = Cursors.Default;
-        } // Add Clients information from DB to listView 
+        } // Add Clients information from DB to listView */
 
-        private void Button_Add_Client_Click(object sender, EventArgs e) // Add new Client
+        /*private void Button_Add_Client_Click(object sender, EventArgs e) // Add new Client
         {
-            Window_Add_Client Add_Client= new Window_Add_Client();
+            Window_Add_Client Add_Client = new Window_Add_Client();
             Add_Client.ShowDialog();
-        }
+        }*/
 
         private void Button_DataMod_Client_Click(object sender, EventArgs e) // Modify Client information
         {
@@ -207,7 +203,7 @@ namespace E_elektryk
             }
         }
 
-        private void Offer_List_Update()
+        /*private void Offer_List_Update()
         {
             Cursor.Current = Cursors.WaitCursor;
             using (zlecenieEntities db = new zlecenieEntities())
@@ -235,13 +231,13 @@ namespace E_elektryk
                 }
             }
             Cursor.Current = Cursors.Default;
-        } // Update Offer information from DB to listView
+        } // Update Offer information from DB to listView*/
 
-        private void Button_Add_Offer_Click(object sender, EventArgs e)
+        /*private void Button_Add_Offer_Click(object sender, EventArgs e)
         {
                 Window_Add_Offer offer = new Window_Add_Offer();
                 offer.ShowDialog();
-        } // Add new offer
+        } // Add new offer*/
 
         private void Button_Modify_Offer_Click(object sender, EventArgs e)
         {
@@ -258,7 +254,7 @@ namespace E_elektryk
             }
         } // Modify offer information
 
-        private void Offer_list_SelectedIndexChanged_1(object sender, EventArgs e)
+        /*        private void Offer_list_SelectedIndexChanged_1(object sender, EventArgs e)
         {
             using (zlecenieEntities db = new zlecenieEntities())
             {
@@ -303,6 +299,124 @@ namespace E_elektryk
                 sum_w_taxes = Math.Round(sum_w_taxes, 2);
                 sum_w_taxes_label_2.Text = "Suma Brutto: " + sum_w_taxes.ToString() + " zł";
             }
-        } // Calculate gross price for offer
+        } // Calculate gross price for offer*/
+
+        private void button_Exit_Window_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        } // Exit Program
+
+        private void button_minimalize_window_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        } // Minimalize Window
+
+        private void Menu_Design()
+        {
+            panel_Order.Visible = false;
+            panel_Offer.Visible = false;
+            panel_Client.Visible = false;
+            panel_Product.Visible = false;
+            panel_Invoice.Visible = false;
+        }
+
+        private void Hide_Sub_Menu()
+        {
+            if (panel_Order.Visible == true)
+                panel_Order.Visible = false;
+            if (panel_Offer.Visible == true)
+                panel_Offer.Visible = false;
+            if (panel_Client.Visible == true)
+                panel_Client.Visible = false;
+            if (panel_Product.Visible == true)
+                panel_Product.Visible = false;
+            if (panel_Invoice.Visible == true)
+                panel_Invoice.Visible = false;
+        }
+
+        private void Show_Sub_Menu(Panel panel)
+        {
+            if (panel.Visible == false)
+            {
+                Hide_Sub_Menu();
+                panel.Visible = true;
+            }
+            else
+            {
+                panel.Visible = false;
+            }
+        }
+
+        private void button_Order_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Order_window());
+            Show_Sub_Menu(panel_Order);
+        } // Open Order Window
+
+        private void button_Offer_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Offer_Window());
+            Show_Sub_Menu(panel_Offer);
+        } // Open Offer Window
+
+        private void button_Client_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Client_Window());
+            Show_Sub_Menu(panel_Client);
+        } // Open Client Window
+
+        private void button_Product_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Product_Window(Selected_Product_ID));
+            Show_Sub_Menu(panel_Product);
+        } // Open Product Window
+
+        private void button_Invoice_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Invoice_Window());
+            Show_Sub_Menu(panel_Invoice);
+        } // Open Invoice Widnow
+
+        private Form activeForm = null;
+
+        private void OpenChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel_Main_Window.Controls.Add(childForm);
+            panel_Main_Window.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        } // Open new window in main window panel
+
+        private void button_New_Offer_Click(object sender, EventArgs e) // Open New Offer Window
+        {
+            OpenChildForm(new Window_Add_Offer());
+            Hide_Sub_Menu();
+        }
+
+        private void Button_New_Product_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Window_Add_Product());
+            Hide_Sub_Menu();
+        } // Open New Product Window
+
+        private void Buton_New_client_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Window_Add_Client());
+            Hide_Sub_Menu();
+        } // Open New Client Window
+
+        private void button_Modify_Product_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Selected_Product_ID.ToString());
+            //OpenChildForm(new Window_Add_Product(Selected_Product_ID));   
+        }
     }
 }
